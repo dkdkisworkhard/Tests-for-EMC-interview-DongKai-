8647ed9e70f5ca68e4158c0d4bd29bd145f04261
dkdkisworkhard <dk130182@126.com>
2017-02-14 10:41:49 +0800

    Update Test1.java

--- test3/emctest3/src/com/dk/emcinterview/test3/Test1.java
+++ test3/emctest3/src/com/dk/emcinterview/test3/Test1.java
@@ -1,66 +1,96 @@
 package com.dk.emcinterview.test3;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("UserInfoService")
-public class Test1 {
-	public static HashMap<String, Integer> LUNlist = new HashMap<String, Integer>();
-	
-	//create lun: http://192.168.1.6:8080/emctest3/rest/StorageArray/createlun?lunname=dk&lunsize=100
-	@GET
-	@Path("/createlun")
-	@Produces(MediaType.TEXT_XML)
-	public synchronized String createLUN(@QueryParam("lunname")String lunname, @QueryParam("lunsize")int lunsize){
-		if(LUNsize.containsKsy(lunname)){
-			return "<createlun>" + lunname + " is exist, can't create a new one " + </createlun>;
-		}else{
-			LUNlist.put(lunname, lunsize);
-		        return "<createlun>" + "Success!!" + lunname + " has been created!!!" + "</createlun>";
-		}
-	}
-	
-	//resizelun: http://192.168.1.6:8080/emctest3/rest/StorageArray/resizelun?lunname=dk&lunsize=200
-	@GET
-	@Path("/resizelun")
-	@Produces(MediaType.TEXT_XML)
-	public synchronized String resizeLUN(@QueryParam("lunname")String lunname, @QueryParam("lunsize")int lunsize){
-		if(LUNsize.containsKsy(lunname)){
-		        LUNlist.put(lunname, lunsize);
-			return "<resizelun>" + "The new size of " + lunname + " is " + LUNlist.get(lunname) + </resizelun>;
-		}else{
-		        return "<resizelun>" + lunname + "is not exist, can't reset it's size!!!" + "</resizelun>";
-		}
-	}
-	
-	//retrieve lun: http://192.168.1.6:8080/emctest3/rest/StorageArray/retrievelun?lunname=dk
-	@GET
-	@Path("/retrievelun")
-	@Produces(MediaType.TEXT_XML)
-	public String retrieveLUN(@QueryParam("lunname")String lunname){
-		if(LUNsize.containsKsy(lunname)){
-		        
-			return "<retrievelun>" + "The size of " + lunname + " is " + LUNlist.get(lunname) + </retrievelun>;
-		}else{
-		        return "<retrievelun>" + lunname + "is not exist, can't retrieve it's size!!!" + "</retrievelun>";
-		}
-	}
-	
-	//remove lun: http://192.168.1.6:8080/emctest3/rest/StorageArray/removelun?lunname=dk
-	@GET
-	@Path("/removelun")
-	@Produces(MediaType.TEXT_XML)
-	public synchronized String removeLUN(@QueryParam("lunname")String lunname){
-		if(LUNsize.containsKsy(lunname)){
-		        LUNlist.remove(lunname);
-			return "<removelun>" + "The lun " + lunname + "has been removed!!!" + </removelun>;
-		}else{
-		        return "<removelun>" + lunname + "is not exist, can't remove!!!" + "</removelun>";
-		}
-	}
-	
-	
+
+@Path("StorageArray")
+public class Test3 {
+        public static HashMap<String , Integer> LUNlist = new HashMap<String, Integer>();
+        
+        //http://localhost:8080/emctest3/rest/StorageArray/createlun?lunname=dk&lunsize=100
+        @GET
+        @Path("/createlun")
+        @Produces(MediaType.TEXT_XML)
+        public synchronized String createLUN(@QueryParam("lunname") String lunname,
+                        @QueryParam("lunsize") int lunsize) {
+                if (LUNlist.containsKey(lunname)) {
+                        return "<lun>" + "The lun " + lunname
+                                        + " is exist, not need to create a new one!!! " + "</lun>";
+                } else {
+
+                        LUNlist.put(lunname, lunsize);
+                        return "<lun>" + "lun " + lunname + " has been created, it's size is " + lunsize
+                                        + "</lun>";
+                }
+        }
+        
+        
+        //http://localhost:8080/emctest3/rest/StorageArray/resizelun?lunname=dk&lunsize=200
+        @GET
+        @Path("/resizelun")
+        @Produces(MediaType.TEXT_XML)
+        public synchronized String resizeLUN(@QueryParam("lunname")String lunname,
+                                        @QueryParam("lunsize")int newlunsize){
+                if (LUNlist.containsKey(lunname)) {
+                        
+                LUNlist.put(lunname, newlunsize);
+                return  "<newlun>" + "lunname: " + lunname + "  lunsize: " + LUNlist.get(lunname) + "</newlun>";
+        
+                }else {
+                        return  "<newlun>" + "lun " + lunname + " is not exist!!! " + "</newlun>";
+                }
+        }
+        
+        //http://localhost:8080/emctest3/rest/StorageArray/removelun?lunname=dk
+        @GET
+        @Path("/removelun")
+        @Produces(MediaType.TEXT_XML)
+        public synchronized String removeLUN(@QueryParam("lunname")String lunname){
+                if (LUNlist.containsKey(lunname)) {
+                        LUNlist.remove(lunname);
+                        if (LUNlist.containsKey(lunname)) {
+                                return "<removelun>" + "remove " + lunname + " failed!!!"
+                                                + "</removelun>";
+                        } else {
+                                return "<removelun>" + "Success!! " + lunname
+                                                + " has been removed!" + "</removelun>";
+                        }
+                }else {
+                        return "<removelun>" + lunname
+                                        + " id not exist, no need to remove!" + "</removelun>";
+                }
+        }
+        
+        //http://localhost:8080/emctest3/rest/StorageArray/retrievelun?lunname=dk
+        @GET
+        @Path("/retrievelun")
+        @Produces(MediaType.TEXT_XML)
+        public String retrieveLUN(@QueryParam("lunname")String lunname){
+                if (LUNlist.containsKey(lunname)) {
+                        return "<retrievelun>" + "The size of " + lunname + " is "+LUNlist.get(lunname) +"</retrievelun>";
+                }else {
+                        return "<removelun>" + lunname
+                                        + " id not exist, can't retrieve it's size!" + "</removelun>";
+                }
+        }
+        
+        public static void main(String[] args) {
+//              LUNlist.put("123", 123);
+                Iterator<String> iter = LUNlist.keySet().iterator();
+                while (iter.hasNext()) {
+                        System.out.println(LUNlist.get(iter.next()));
+                }
+        }
+        
+        
+        
 }
